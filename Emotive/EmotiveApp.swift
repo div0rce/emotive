83ffad6417d01330296f4fef18be103acc828fff
Mous <156965421+div0rce@users.noreply.github.com>
2025-10-01 17:39:04 -0400

    Initial Commit

---
+++ Emotive/EmotiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotiveApp.swift
+//  Emotive
+//
+//  Created by moustafa on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EmotiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
